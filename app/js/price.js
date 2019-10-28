a276e0fe700adb0b343f156ae581b30c607ec66a
MirraR1990 <46891385+MirraR1990@users.noreply.github.com>
2019-10-29 02:19:09 +0300

    commit

--- app/js/price.js
+++ app/js/price.js
@@ -1,47 +1,47 @@
 $(function() {
+    let target; 
+    let elem;
+    let buttonBorder = {border: '2px solid rgb(255, 255, 255)'};
+    let targetBorder = {border: '3px solid rgb(255, 0, 0)'};
     
     $('.costCalculation button').bind('click', function() {
-        let target = $(this);
-        let elem =  target.attr("id");
-        let parent = $(this).parent().attr('class');
-        console.log(parent);
+        target = $(this);
+        elem =  target.attr("id");
        switch(elem) {
            case "adults": 
            case "pupils": 
            case "preschoolers": $(".teacher").css('display', 'block');
-                                target.css('border', '2px solid rgb(147, 112, 219)');
-                                borderColor(target, parent);
+                                $('.age button').css(buttonBorder);
+                                target.css(targetBorder);
+                                
                                 
            break;
            case "russianSpeaker": 
            case "native":  $(".course").css('display', 'block');
+                           $('.teacher button').css(buttonBorder);
+                           target.css(targetBorder);
            break;
            case "miniGroup": 
            case "bigGroup": 
            case "privatelLessons": 
            case "examPreparation":
            case "internationalExam": $(".intensity").css('display', 'block');
+                                     $('.course button').css(buttonBorder);
+                                     target.css(targetBorder);
            break;
            case "threeHours": 
            case "fourHours": 
            case "sixHours":  $(".days").css('display', 'block');
+                             $('.intensity button').css(buttonBorder);
+                             target.css(targetBorder);
            break;
            case "weekdays": 
            case "weekend": 
-           case "weekdaysAndWeekend": alert("All");
+           case "weekdaysAndWeekend": $('.days button').css(buttonBorder);
+                                      target.css(targetBorder);
            break;
        }
        
     });
-
-    function borderColor(elemId, elemClass) { };
-
-    
-
-    
-    
-
-
-
 
 });
--- dist/js/price.js
+++ dist/js/price.js
@@ -1,40 +1,52 @@
 $(function() {
+    let parent;
+    let target; 
     
     $('.costCalculation button').bind('click', function() {
-        let target = $(this);
+        target = $(this);
         let elem =  target.attr("id");
        switch(elem) {
            case "adults": 
            case "pupils": 
            case "preschoolers": $(".teacher").css('display', 'block');
+                                parent = $('.age');
+                                borderColor(target, parent);
+                                
            break;
            case "russianSpeaker": 
            case "native":  $(".course").css('display', 'block');
+                           parent = $('age');
            break;
            case "miniGroup": 
            case "bigGroup": 
            case "privatelLessons": 
            case "examPreparation":
            case "internationalExam": $(".intensity").css('display', 'block');
+                                    parent = $('age');
            break;
            case "threeHours": 
            case "fourHours": 
            case "sixHours":  $(".days").css('display', 'block');
+                            parent = $('age');
            break;
            case "weekdays": 
            case "weekend": 
            case "weekdaysAndWeekend": alert("All");
+                                    parent = $('age');
            break;
        }
        
     });
 
-    
-
-    
-    
-
-
-
-
+    function borderColor(elemId, parent) {
+        let array = parent.children();
+        for( let i = 0; i < array.length - 1; i++) {
+            console.log(array[i]);
+            if(array[i] === target) {
+                console.log("h");
+            }
+        }
+       
+        elemId.css('border', '2px solid rgb(147, 112, 219)');
+    };
 });
